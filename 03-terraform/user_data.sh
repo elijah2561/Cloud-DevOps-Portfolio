4b505725b6c07f6f13bbe2b9ebb24df07bfa7ba0
elijah2561 <elimateoakida@gmail.com>
2026-01-01 17:12:59 +0300

    updated user_data

--- 03-terraform/user_data.sh
+++ 03-terraform/user_data.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
+
+
 set -e
 
-# Create sysmonitor user
-useradd -m -r -s /bin/bash sysmonitor
+# Install dependencies
+apt-get update -y
+apt-get install -y git
+
+# Create service user
+id sysmonitor || useradd -r -s /bin/false sysmonitor
 
 # Create directories
 mkdir -p /opt/sysmonitor/bin
 mkdir -p /var/log/sysmonitor
 
-# Set ownership
-chown -R sysmonitor:sysmonitor /opt/sysmonitor
-chown -R sysmonitor:sysmonitor /var/log/sysmonitor
+# Clone repo
+cd /opt
+git clone https://github.com/elijah2561/Cloud-DevOps-portfolio.git
+
+# Copy script
+cp Cloud-DevOps-portfolio/01-shell-scripting/system_health_check.sh /opt/sysmonitor/bin/
+
+# Permissions
+chown -R sysmonitor:sysmonitor /opt/sysmonitor /var/log/sysmonitor
+chmod 750 /opt/sysmonitor/bin
+chmod 750 /var/log/sysmonitor
+chmod 750 /opt/sysmonitor/bin/system_health_check.sh
+
+# Cron job
+echo "*/5 * * * * /opt/sysmonitor/bin/system_health_check.sh" | crontab -u sysmonitor -
+
+EOF
 
 # Basic logging
 echo "Bootstrap completed at $(date)" >> /var/log/sysmonitor/bootstrap.log
